8dfea5940956bc4e97b80e4a52ffbb008b93c153
deveshchoudhari <deveshch2@gmail.com>
2024-02-26 19:05:25 +0530

    made some component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http'
+import { HeaderComponent } from './header/header/header.component';
+import { ProductsComponent } from './products/products/products.component';
+import { CartComponent } from './cart/cart/cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ProductsComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
