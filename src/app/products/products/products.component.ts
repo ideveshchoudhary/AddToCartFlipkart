57a5200cd8642e91a7ba280b3af807c3340d0521
ideveshchoudhary <deveshch2@gmail.com>
2024-02-27 09:13:38 +0530

    api not working in products

--- src/app/products/products/products.component.ts
+++ src/app/products/products/products.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiServiceService } from 'src/app/services/api-service.service';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.scss']
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
+  products: any[] = [];
+  constructor(private service: ApiServiceService) {
+
+  }
+  ngOnInit(): void {
+
+  }
+
+  getTheProducts() {
+    this.service.getAllProducts().subscribe((res: any) => {
+      this.products = res;
+      console.log("Api wroking", this.products);
+    }
+    )
+  }
+
 
 }
--- src/app/services/api-service.service.ts
+++ src/app/services/api-service.service.ts
@@ -1,9 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiServiceService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAllProducts(): Observable<any[]> {
+    return this.http.get<any[]>("https://fakestoreapi.com/products/")
+  }
 }
